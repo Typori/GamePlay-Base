cef8d922d830359925561336745812b7c1ee868b
Typori <383103897@qq.com>
2024-02-05 18:00:54 +0800

    基础操作ok

--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -22,16 +22,42 @@ namespace TyporiGame
 
         [Tooltip("加速度")] public float SpeedChangeRate = 10.0f;
 
+        [Space(10)] [Tooltip("The height the player can jump")]
+        public float JumpHeight = 1.2f;
+
+        [Tooltip("The character uses its own gravity value. The engine default is -9.81f")]
+        public float Gravity = -15.0f;
+
+        [Space(10)] [Tooltip("Time required to pass before being able to jump again. Set to 0f to instantly jump again")]
+        public float JumpTimeout = 0.50f;
+
+        [Tooltip("Time required to pass before entering the fall state. Useful for walking down stairs")]
+        public float FallTimeout = 0.15f;
+
+        [Header("Player Grounded")] [Tooltip("独立的地面检测")]
+        public bool Grounded = true;
+
+        [Tooltip("Useful for rough ground")] public float GroundedOffset = -0.14f;
+
+        [Tooltip("The radius of the grounded check. Should match the radius of the CharacterController")]
+        public float GroundedRadius = 0.28f;
+
+        [Tooltip("What layers the character uses as ground")]
+        public LayerMask GroundLayers;
+
 
         private PlayerInput _playerInput;
         private GameObject _mainCamera;
-        private StarterAssetsInputs _input;
+        private PlayerInputs _input;
         private CharacterController _controller;
         private float _speed;
         private float _targetRotation = 0.0f;
         private float _rotationVelocity;
         private float _verticalVelocity;
+        private float _terminalVelocity = 53.0f;
 
+        private float _jumpTimeoutDelta;
+        private float _fallTimeoutDelta;
 
         private void Awake()
         {
@@ -43,12 +69,18 @@ namespace TyporiGame
 
         void Start()
         {
-            _input = GetComponent<StarterAssetsInputs>();
+            _input = GetComponent<PlayerInputs>();
             _controller = GetComponent<CharacterController>();
+
+            // reset our timeouts on start
+            _jumpTimeoutDelta = JumpTimeout;
+            _fallTimeoutDelta = FallTimeout;
         }
 
         void Update()
         {
+            JumpAndGravity();
+            GroundedCheck();
             Move();
         }
 
@@ -97,6 +129,69 @@ namespace TyporiGame
 
             //移动角色
             _controller.Move(targetDirection.normalized * (_speed * Time.deltaTime) + new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
+        }
+
+        private void GroundedCheck()
+        {
+            // set sphere position, with offset 好像是设置一个球形碰撞体作为当前物体，看在Layer中是否有物体碰到
+            Vector3 spherePosition = new Vector3(transform.position.x, transform.position.y - GroundedOffset, transform.position.z);
+            Grounded = Physics.CheckSphere(spherePosition, GroundedRadius, GroundLayers, QueryTriggerInteraction.Ignore);
+        }
+
+        /// <summary>
+        /// 这里面只计算了纵向加速度，最终的移动操作还是放在Move中一块进行了
+        /// </summary>
+        private void JumpAndGravity()
+        {
+            if (Grounded)
+            {
+                // reset the fall timeout timer
+                _fallTimeoutDelta = FallTimeout;
+
+                // stop our velocity dropping infinitely when grounded
+                if (_verticalVelocity < 0.0f)
+                {
+                    _verticalVelocity = -2f;
+                }
+
+                // Jump
+                if (_input.jump && _jumpTimeoutDelta <= 0.0f)
+                {
+                    // H * -2 * G的平方 = 达到目标高度所需的速度，由已知加速度算总路程的公式d=1/2(a(t^2))可以推出
+                    _verticalVelocity = Mathf.Sqrt(JumpHeight * -2f * Gravity);
+                }
+
+                // jump timeout
+                if (_jumpTimeoutDelta >= 0.0f)
+                {
+                    _jumpTimeoutDelta -= Time.deltaTime;
+                }
+            }
+            else
+            {
+                // reset the jump timeout timer
+                _jumpTimeoutDelta = JumpTimeout;
+
+                // 只是为了记录下落时间，以切换动画
+                if (_fallTimeoutDelta >= 0.0f)
+                {
+                    _fallTimeoutDelta -= Time.deltaTime;
+                }
+                else
+                {
+                    // update animator if using character
+                }
+
+                //不在地面上，就要把跳跃设为关闭
+                _input.jump = false;
+            }
+
+            // apply gravity over time if under terminal (multiply by delta time twice to linearly speed up over time)
+            //纵向速度受重力影响，但设置了一个最大值。解释中的twice没懂
+            if (_verticalVelocity < _terminalVelocity)
+            {
+                _verticalVelocity +=  Gravity * Time.deltaTime;
+            }
         }
     }
 }
